b3b3735f9fa73593b699d73b884b626458b0fc8b
minhquantran <tranminhquan027@gmail.com>
2022-09-29 16:35:03 +0700

    public proxy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://adfccc8efccac4ce0acb7fbc0b39da5a-867835637.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
